bd2b161d18244dd838d71dd9b973e7e2effafc67
Pär Björklund <per.bjorklund@gmail.com>
2019-11-26 15:26:51 +0100

    WIP

--- BuildAllPlatforms.ps1
+++ BuildAllPlatforms.ps1
@@ -5,26 +5,33 @@ Param(
   [switch] $GenerateProjects,
   [switch] $Rel = $false,
   [switch] $Deb = $false,
-  [switch] $Rebuild = $false
+  [switch] $Rebuild = $false,
+  [switch] $Desktop = $false,
+  [switch] $App = $false,
+  [ValidateSet( 'arm', 'win32', 'x64', 'arm64' )]
+  [string[]] $Platforms = @( 'arm', 'win32', 'x64', 'arm64' )
 )
 
 $ErrorActionPreference = "Stop"
 
-$platforms = @( 'arm', 'win32', 'x64', 'arm64' )
 
 if ($GenerateProjects) {
   foreach ($platform in $platforms) {
-    $path = "$PsScriptRoot\Build\$platform"
-    if (!(Test-Path $path)) {
-      New-Item $path -ItemType Directory
+    if ($Desktop) {
+      $path = "$PsScriptRoot\Build\$platform"
+      if (!(Test-Path $path)) {
+        New-Item $path -ItemType Directory
+      }
+      cmake -G "Visual Studio 15" -A $platform -Thost=x64 -S $PsScriptRoot -B $path
     }
-    cmake -G "Visual Studio 16" -A $platform -Thost=x64 -S $PsScriptRoot -B $path
 
-    $path = "$PsScriptRoot\Build\win10-$Platform"
-    if (!(Test-Path $path)) {
-      New-Item $path -ItemType Directory
+    if ($App) {
+      $path = "$PsScriptRoot\Build\win10-$Platform"
+      if (!(Test-Path $path)) {
+        New-Item $path -ItemType Directory
+      }
+      cmake -G "Visual Studio 15" -A $platform -Thost=x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION='10.0.17763.0' -S $PsScriptRoot -B $path
     }
-    cmake -G "Visual Studio 16" -A $platform -Thost=x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION='10.0' -S $PsScriptRoot -B $path
   }
 }
 
@@ -35,26 +42,30 @@ if ($null -ne $Packages) {
   }
   foreach ($package in $Packages) {
     foreach ($platform in $platforms) {
-      $path = "$PsScriptRoot\Build\$platform"
-      if ($Deb) {
-        cmake --build $path --config Debug -t $package $cleanFirst
-        if ($LASTEXITCODE -ne 0) { return; }
+      if ($Desktop) {
+        $path = "$PsScriptRoot\Build\$platform"
+        if ($Deb) {
+          cmake --build $path --config Debug -t $package $cleanFirst
+          if ($LASTEXITCODE -ne 0) { return; }
+        }
+
+        if ($Rel) {
+          cmake --build $path --config RelWithDebInfo -t $package $cleanFirst
+          if ($LASTEXITCODE -ne 0) { return; }
+        }
       }
 
-      if ($Rel) {
-        cmake --build $path --config RelWithDebInfo -t $package $cleanFirst
-        if ($LASTEXITCODE -ne 0) { return; }
-      }
+      if ($App) {
+        $storePath = "$PsScriptRoot\Build\win10-$Platform"
+        if ($Deb) {
+          cmake --build $storePath --config Debug -t $package $cleanFirst
+          if ($LASTEXITCODE -ne 0) { return; }
+        }
 
-      $storePath = "$PsScriptRoot\Build\win10-$Platform"
-      if ($Deb) {
-        cmake --build $storePath --config Debug -t $package $cleanFirst
-        if ($LASTEXITCODE -ne 0) { return; }
-      }
-
-      if ($Rel) {
-        cmake --build $storePath --config RelWithDebInfo -t $package $cleanFirst
-        if ($LASTEXITCODE -ne 0) { return; }
+        if ($Rel) {
+          cmake --build $storePath --config RelWithDebInfo -t $package $cleanFirst
+          if ($LASTEXITCODE -ne 0) { return; }
+        }
       }
     }
   }
